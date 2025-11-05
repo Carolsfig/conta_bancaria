2bf2cfe627bc384850b0501b5b4ba165d67409b1
Carolina Figueiredo <carolinasfigueiredo@hotmail.com>
2025-11-05 14:59:30 -0300

    Construção do Projeto e Menu Principal

---
+++ Menu.ts
@@ -0,0 +1,18 @@
+
+console.log(`
+*******************************************s*****\n
+             BANCO DO BRAZIL COM Z\n
+************************************************`)
+console.log(`
+         1 - Criar Conta
+         2 - Listar todas as Contas
+         3 - Buscar conta por número
+         4 - Atualizar Dados da Conta
+         5 - Apagar Conta
+         6 - Sacar
+         7 - Depositar
+         8 - Transferir valores entre Contas
+         9 - Sair`)
+
+console.log("\n************************************************")
+console.log("Entre com a opção desejada: ")
